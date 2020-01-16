c9bb5e386f13b6cb22a9a94ad55e331e850e3ed5
Alvin <alvindsp@gmail.com>
2020-01-16 14:46:59 -0500

    Created models directory and added Company class Employers now displays ListView of companies

--- McGillStartupFair/Employers.xaml.cs
+++ McGillStartupFair/Employers.xaml.cs
@@ -1,15 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using McGillStartupFair.Models;
 
 using Xamarin.Forms;
 
 namespace McGillStartupFair
-{
+{ 
     public partial class Employers : ContentPage
     {
+        public IList<Company> companies { get; private set; }
+
         public Employers()
         {
+
             InitializeComponent();
+
+            companies = new List<Company>();
+
+
+            companies.Add(new Company
+            {
+                Name = "Google",
+                Description = "Search Engine Startup",
+                URL = "www.google.com"
+            });
+
+            companies.Add(new Company
+            {
+                Name = "Amazon",
+                Description = "Online Marketplace and Ruthless Regime",
+                URL = "www.amazon.com"
+            });
+
+            companies.Add(new Company
+            {
+                Name = "Netflix",
+                Description = "Streaming Media",
+                URL = "www.netflix.com"
+            });
+
+            companies.Add(new Company
+            {
+                Name = "Oracle",
+                Description = "tech comp",
+                URL = "www.oracle.com"
+            });
+
+            BindingContext = this;
+
         }
     }
 }
